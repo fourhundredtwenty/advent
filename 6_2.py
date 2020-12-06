7566bb069341ecb1e269b9c9647ce28554b3866d
lx karpinski <me@alexkarpinski.com>
2020-12-06 12:45:00 -0500

    slightly better formatting

--- 6_2.py
+++ 6_2.py
@@ -5,4 +5,11 @@ groups_of_forms = raw_forms.split("\n\n")
 
 groups_of_people = [[person for person in group.split('\n') if person] for group in groups_of_forms]
 
-print(sum([len(set("".join(["".join([letter for letter in person if all(letter in other_person for other_person in group)]) for person in group]))) for group in groups_of_people]))
+print(sum(
+    [len(set("".join(
+        ["".join(
+            [letter for letter in person
+             if all(letter in other_person for other_person in group)]
+        ) for person in group]
+    ))) for group in groups_of_people]
+))
