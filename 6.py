475b4a7ddc1a9a97abb083758a6a8773d4283cd0
lx karpinski <me@alexkarpinski.com>
2020-12-06 13:18:38 -0500

    repo perfected

--- 6.py
+++
@@ -1,9 +0,0 @@
-with open("6.data") as datafile:
-    raw_forms = datafile.read()
-
-groups_of_forms = raw_forms.split("\n\n")
-
-yes_count_per_group = [len(set(group.replace('\n', ''))) for group in groups_of_forms]
-total_yesses = sum(yes_count_per_group)
-
-print(total_yesses)
---
+++ README.md
@@ -0,0 +1,2 @@
+![stunned_pikachu](https://i.kym-cdn.com/entries/icons/facebook/000/027/475/Screen_Shot_2018-10-25_at_11.02.15_AM.jpg)
+
